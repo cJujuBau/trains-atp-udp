f4a32b8adceb8d4cd6ca1900da533b8f1f7151a2
MathieuH35 <teutonium35@gmail.com>
2024-12-13 19:40:08 +0100

    clean: remove excess code from server

--- server.c
+++ server.c
@@ -70,37 +70,12 @@ int main(int argc, char * argv[]) {
 
     printf("Adrlect %d port %d\n", adrlect.sin_addr.s_addr, adrlect.sin_port);
     
-    
     trainData trainRegister[TRACKLENGTH];
     
     init_register(trainRegister);
-
-    add_train(trainRegister, "TGV0");
-
-    display_trains(trainRegister);
-
-    move_train(trainRegister, 0,10);
-
-    add_train(trainRegister, "TGV1");
-
-    move_train(trainRegister, 10, 50);
-
-    move_train(trainRegister, 0, 30);
-
-    add_train(trainRegister, "TER0");
-
-    move_train(trainRegister, 0, 100);
-
-    move_train(trainRegister, 400, 700);
-    
-    parse_msg(trainRegister, "TGV0:4:50:150:");
-
-    fprintf(stderr, "test");
     
     while (1){
         char buffer[MAXOCTETS+1] = {""};
-
-        printf("buffer is %s", buffer);
 
         scan_msg(&buffer, &adrecriv, &sd);
         
@@ -109,6 +84,5 @@ int main(int argc, char * argv[]) {
         display_trains(trainRegister);
 
     }
-    
     exit(EXIT_SUCCESS);
 }
